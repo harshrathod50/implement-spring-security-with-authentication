fb4c6f718a2f6a003102cab4d5773a71a66bf0f5
Harsh Rathod <hrathore50@ymail.com>
2023-06-22 13:50:35 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/implementspringsecuritywithauthentication/ImplementSpringSecurityWithAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.implementspringsecuritywithauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementSpringSecurityWithAuthenticationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImplementSpringSecurityWithAuthenticationApplication.class, args);
+  }
+}
